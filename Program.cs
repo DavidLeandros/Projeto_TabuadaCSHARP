2c7c50a429c497a307713533a6d3146b04565708
Carol <dleandrosilva@hotmail.com>
2022-07-18 10:34:17 -0300

    Exercicio calculadora csharp

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Nova_pasta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o numero da tabuada: ");
+            int tabu = int.Parse(Console.ReadLine());
+            int result = 0;
+
+            Console.WriteLine("\n" + "A tabuada do " + tabu + " é: \n" );
+
+            for(int x = 0; x < 11; x++)
+            {
+                result = tabu * x;
+
+                Console.WriteLine(tabu + "x" + x + " = " + result + "\n");
+
+            }
+        }
+    }
+}
